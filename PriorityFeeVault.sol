9c0ec7d5f91fe13cd63cebbf8ec410deeff68487
Severin Araujo <165962430+severinCRO@users.noreply.github.com>
2024-10-24 20:44:28 -0300

    Add files via upload

---
+++ PriorityFeeVault.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import "./FeeVault.sol";
+
+/// @title Priority fee accumulator contract
+/// @author Citrea
+
+contract PriorityFeeVault is FeeVault {}
